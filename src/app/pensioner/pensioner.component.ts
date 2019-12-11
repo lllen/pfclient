5e107c2b7ba7f75f898ed4b311f614b40909e0ff
nabuhabu <nastyuha_sh@ukr.net>
2019-12-12 00:04:31 +0200

    pensioner and setting added

---
+++ src/app/pensioner/pensioner.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pensioner',
+  templateUrl: './pensioner.component.html',
+  styleUrls: ['./pensioner.component.scss']
+})
+export class PensionerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
