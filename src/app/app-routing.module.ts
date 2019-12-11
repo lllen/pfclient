9e557f4564248c3863857b0b2c2407d99865a0ae
nabuhabu <nastyuha_sh@ukr.net>
2019-12-12 00:15:21 +0200

    pensioner improved

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,29 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {PensionersComponent} from "./pensioners/pensioners.component";
+import {PensionerComponent} from "./pensioner/pensioner.component";
+import {PensionerAddFormComponent} from "./pensioner-add-form/pensioner-add-form.component";
+import {MainComponent} from "./main/main.component";
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'pensioner/add',
+    component: PensionerAddFormComponent,
+  },
+  {
+    path: 'pensioners',
+    component: PensionersComponent,
+  },
+  {
+    path: 'pensioner/:id',
+    component: PensionerComponent,
+  },
+  {
+    path: '',
+    component: MainComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/pensioner/pensioner.component.ts
+++ src/app/pensioner/pensioner.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute, ParamMap, Router} from "@angular/router";
+import {AppService} from "../app.service";
+import {SnackbarService} from "../snackbar.service";
 
 @Component({
   selector: 'app-pensioner',
@@ -7,9 +10,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PensionerComponent implements OnInit {
 
-  constructor() { }
+  pensioner;
+
+  constructor(private router: Router,
+              private activatedRoute: ActivatedRoute,
+              private appService: AppService,
+              private messageService: SnackbarService) { }
 
   ngOnInit() {
+    this.activatedRoute.paramMap.subscribe((params: ParamMap) => {
+      this.appService.getPensioner(params.get('id')).subscribe(data => {
+        console.log(data);
+        this.pensioner = data;
+        this.pensioner.years = (new Date()).getFullYear() - new Date(this.pensioner.dateOfBirth).getFullYear();
+        console.log(this.pensioner.sex);
+      })
+    });
+  }
+
+  handleDelete(id) {
+    this.appService.deletePensioner(id).subscribe(data => {
+      if(data) {
+        this.messageService.showSnackBar({
+          message: 'Успішно',
+          isError: false
+        });
+        this.router.navigate(['/pensioners'])
+      }
+    },
+        error1 => {
+          this.messageService.showSnackBar({
+            message: 'Людина з досвідом роботи НЕ МОЖЕ бути видалена',
+            isError: true
+          });
+        },
+      ()=>{}
+    );
   }
 
 }
